bb1642add3da186cb5a1ccf422f5bdb9347539bf
=silasdev_primary <=silasmaques@outlook.com>
2023-06-01 21:33:37 -0300

    Primeiras Alterações no projeto

--- app/src/main/java/devandroid/silas/AppMinhaLista/MainActivity.java
+++ app/src/main/java/devandroid/silas/AppMinhaLista/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
