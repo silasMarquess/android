1f3fd835d0879a9afd18418a324e4abb8a0911ce
=silasdev_primary <=silasmaques@outlook.com>
2023-06-03 11:11:49 -0300

    Insere Dados No aqruivo SharedPreferences

--- app/src/main/java/devandroid/silas/AppMinhaLista/view/MainActivity.java
+++ app/src/main/java/devandroid/silas/AppMinhaLista/view/MainActivity.java
@@ -3,6 +3,7 @@ package devandroid.silas.AppMinhaLista.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,9 @@ import devandroid.silas.AppMinhaLista.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    SharedPreferences preferences ;
+    public static final String NOME_SHAREDPREFERENCES = "SHARE_INSERIR";
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         int parada =0;
 
-        pessoa = new Pessoa("nome","Marques de Sousa",new Curso("Engenharia de Alimento"),"99981041614");
+        pessoa = new Pessoa("SILAS MARQUES","Marques de Sousa",new Curso("Engenharia de Alimento"),"99981041614");
         String PrimeiroNome = pessoa.getNome();
         String segundoNome = pessoa.getSobrenome();
+
+        preferences = getSharedPreferences(NOME_SHAREDPREFERENCES,0);
+        SharedPreferences.Editor ListaEdit = preferences.edit();
+
+
 
 
         //LOGANDO TEXTO NA TELA DA VIWER
@@ -40,10 +49,10 @@ public class MainActivity extends AppCompatActivity {
         Button btn_Finalizar = findViewById(R.id.btn_Finalizar);
 
 
-        txt_nome.setText(pessoa.getNome().toUpperCase());
+       /* txt_nome.setText(pessoa.getNome().toUpperCase());
         txt_sobreNome.setText(pessoa.getSobrenome());
         txt_cursoDesejadoEditText.setText(pessoa.getCurso().getNomeCurso());
-        txt_telefone.setText(pessoa.getTelefone());
+        txt_telefone.setText(pessoa.getTelefone());*/
 
         //EVENTOS
         Button btn_Limpar = findViewById(R.id.btn_Limpar);
@@ -75,13 +84,12 @@ public class MainActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        String nome = txt_nome.getText().toString();
-                        String SobreNome = txt_sobreNome.getText().toString();
-                        String cursoSelecionado = txt_cursoDesejadoEditText.getText().toString();
-                        String telefone = txt_telefone.getText().toString();
 
-                        pessoa = new Pessoa(nome,SobreNome,
-                                new Curso(cursoSelecionado),telefone);
+                        ListaEdit.putString("PrimeiroNome",pessoa.getNome());
+                        ListaEdit.putString("sobreNome",pessoa.getSobrenome());
+                        ListaEdit.putString("cursoSelecionado",pessoa.getCurso().getNomeCurso());
+                        ListaEdit.putString("telefone",pessoa.getTelefone());
+                        ListaEdit.apply();
 
                         Toast.makeText(MainActivity.this,"Dados de: "+pessoa.toString()+" Salvos com sucesso",Toast.LENGTH_LONG).show();
                        // finish();
